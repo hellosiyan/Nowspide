af9a63b0fa6c0ed8209447350441bf98709bac50
Siyan Panayotov <xsisqox@gmail.com>
2010-07-08 21:22:36 +0300

    Add About dialog

--- src/nsp-window.c
+++ src/nsp-window.c
@@ -21,6 +21,7 @@
 #include <assert.h>
 #include <stdlib.h>
 
+static void nsp_window_cmd_about (GtkAction *action, GtkWindow *window);
 static const GtkActionEntry action_entries_window[] = {
     { "File",  NULL, "_File" },
     { "Help",  NULL, "_Help" },
@@ -29,7 +30,7 @@ static const GtkActionEntry action_entries_window[] = {
       "Close Window",NULL },
      
     { "HelpAbout", GTK_STOCK_FILE, "About", "<control>O",
-      "Show About dialog",NULL }
+      "Show About dialog", G_CALLBACK(nsp_window_cmd_about) }
 };
 
 NspWindow * 
@@ -136,4 +137,37 @@ nsp_window_init(NspWindow *win, GError **error)
 	gtk_box_pack_start(GTK_BOX(c2_holder), f_title, FALSE, FALSE, 0);
 	
 	return 0;
+}
+
+static void
+nsp_window_cmd_about (GtkAction *action, GtkWindow *window)
+{
+    static const char *authors[] = {
+        "Programming",
+        "\tSiyan Panayotov <xsisqox@gmail.com>",
+        NULL
+    };
+
+    char *license =
+          ("Nowspide is free software: you can redistribute it and/or modify "
+           "it under the terms of the GNU General Public License as published by "
+           "the Free Software Foundation, either version 3 of the License, or "
+           "(at your option) any later version.\n\n"
+           "Viewnior is distributed in the hope that it will be useful, "
+           "but WITHOUT ANY WARRANTY; without even the implied warranty of "
+           "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the "
+           "GNU General Public License for more details.\n\n"
+           "You should have received a copy of the GNU General Public License "
+           "along with Nowspide.  If not, see <http://www.gnu.org/licenses/>.\n");
+
+    gtk_show_about_dialog (GTK_WINDOW (window),
+                   "program-name", "Nowspide",
+                   "version", "0.1",
+                   "copyright", "Copyright \xc2\xa9 2010 Siyan Panayotov <xsisqox@gmail.com>",
+                   "comments","Handy and modern desktop feed reader",
+                   "authors", authors,
+                   "logo-icon-name", GTK_STOCK_INFO,
+                   "wrap-license", TRUE,
+                   "license", license,
+                   NULL);
 }
